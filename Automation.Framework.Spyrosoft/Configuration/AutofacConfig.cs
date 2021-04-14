d7948c6b430dd948973d6ead3f10b47d1dfc1293
Mariusz Krzywdzinski <Mariusz.Krzywdzinski@fenergo.com>
2021-04-14 22:15:50 +0200

    Structure and configuration added Nuget packages added: - nunit - webdriver - chromedriver v89 - specflow - specflow.autofac - fluentassertions

---
+++ Automation.Framework.Spyrosoft/Configuration/AutofacConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Autofac;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SpecFlow.Autofac;
+using TechTalk.SpecFlow;
+
+namespace Automation.Framework.Spyrosoft.Configuration
+{
+    class AutofacConfig
+    {
+        [ScenarioDependencies]
+        private static ContainerBuilder ContainerConfig()
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterType<ChromeDriver>().As<IWebDriver>().SingleInstance();
+
+            //Registering all test classes from assembly with [Binding] annotation
+            containerBuilder
+                .RegisterTypes(typeof(AutofacConfig).Assembly.GetTypes()
+                    .Where(t => Attribute.IsDefined(t, typeof(BindingAttribute)))
+                    .ToArray())
+                .SingleInstance().InstancePerLifetimeScope();
+
+            return containerBuilder;
+        }
+    }
+}
---
+++ Automation.Framework.Spyrosoft/Configuration/Hooks.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using System.Configuration;
+using TechTalk.SpecFlow;
+
+namespace Automation.Framework.Spyrosoft.Configuration
+{
+    [Binding]
+    public class Hooks
+    {
+        private readonly IWebDriver _driver;
+
+        public Hooks(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        [BeforeScenario]
+        public void SetUp()
+        {
+            _driver.Manage().Window.Maximize();
+            _driver.Url = ConfigurationManager.AppSettings["BaseUrl"];
+        }
+
+        [AfterScenario]
+        public void CleanUp()
+        {
+            _driver.Manage().Cookies.DeleteAllCookies();
+            _driver.Close();
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
